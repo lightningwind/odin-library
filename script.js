983f751b8646a322d6b1c3a12ba819c5f344a650
David Islam <david.islam24@gmail.com>
2023-04-10 05:36:18 -0700

    Set up project with skeleton HTML/CSS/JS files

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages; 
+    this.read = read; 
+}
+
+Book.prototype.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+}
+
+function addBookToLibrary() {
+    // TODO
+}
